ad9560dba5b913b84a2ce6ba779d6d5b1b7489a3
jhoni-costa <jhoni_costa@outlook.com>
2018-10-24 17:15:07 -0300

    Acessando a camera

--- app/src/main/java/br/com/jhonicosta/xapp_messenger/activities/ConfiguracoesActivity.java
+++ app/src/main/java/br/com/jhonicosta/xapp_messenger/activities/ConfiguracoesActivity.java
@@ -2,28 +2,40 @@ package br.com.jhonicosta.xapp_messenger.activities;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
 
 import br.com.jhonicosta.xapp_messenger.R;
 import br.com.jhonicosta.xapp_messenger.helper.Permissions;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
 
+    private static final int CAMERA_ = 100;
+    private static final int GALERIA_ = 200;
+
     private String[] permissions = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA
     };
 
+    private ImageButton camera;
+    private ImageButton galeria;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
+
+        camera = findViewById(R.id.buttonCamera);
+        galeria = findViewById(R.id.buttonGaleria);
 
         Permissions.validarPermissions(permissions, this, 1);
 
@@ -32,6 +44,23 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, CAMERA_);
+                }
+            }
+        });
+
+        galeria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
