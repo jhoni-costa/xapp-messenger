577c76af87cc7312b697494a84cffd506178870d
jhoni-costa <jhoni_costa@outlook.com>
2018-10-26 17:30:35 -0300

    Layout Activity Chat

--- app/src/main/java/br/com/jhonicosta/xapp_messenger/activities/ChatActivity.java
+++ app/src/main/java/br/com/jhonicosta/xapp_messenger/activities/ChatActivity.java
@@ -15,17 +15,10 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
