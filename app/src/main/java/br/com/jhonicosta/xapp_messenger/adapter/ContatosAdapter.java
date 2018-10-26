315eeacfc582624cd27a7c31d4d1059a473854e1
jhoni-costa <jhoni_costa@outlook.com>
2018-10-26 16:35:05 -0300

    Adapter, Recycle View para lista de contatos

---
+++ app/src/main/java/br/com/jhonicosta/xapp_messenger/adapter/ContatosAdapter.java
@@ -0,0 +1,71 @@
+package br.com.jhonicosta.xapp_messenger.adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import br.com.jhonicosta.xapp_messenger.R;
+import br.com.jhonicosta.xapp_messenger.model.Usuario;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ContatosAdapter extends RecyclerView.Adapter<ContatosAdapter.MyViewHolder> {
+
+    private List<Usuario> contatos;
+    private Context context;
+
+    public ContatosAdapter(List<Usuario> list, Context context) {
+        this.contatos = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View itemLista = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.adapter_contatos, viewGroup, false);
+
+        return new MyViewHolder(itemLista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+        Usuario usuario = contatos.get(i);
+        myViewHolder.nome.setText(usuario.getNome());
+        myViewHolder.email.setText(usuario.getEmail());
+
+        if (usuario.getFotoUsuario() != null) {
+            Glide.with(context).load(Uri.parse(usuario.getFotoUsuario())).into(myViewHolder.foto);
+        } else {
+            myViewHolder.foto.setImageResource(R.drawable.padrao);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return contatos.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView foto;
+        TextView nome, email;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            foto = itemView.findViewById(R.id.adapter_foto);
+            nome = itemView.findViewById(R.id.adapter_nome);
+            email = itemView.findViewById(R.id.adapter_email);
+
+        }
+    }
+}
--- app/src/main/java/br/com/jhonicosta/xapp_messenger/fragments/ContatoFragment.java
+++ app/src/main/java/br/com/jhonicosta/xapp_messenger/fragments/ContatoFragment.java
@@ -2,14 +2,32 @@ package br.com.jhonicosta.xapp_messenger.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import br.com.jhonicosta.xapp_messenger.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.jhonicosta.xapp_messenger.R;
+import br.com.jhonicosta.xapp_messenger.adapter.ContatosAdapter;
+import br.com.jhonicosta.xapp_messenger.config.FirebaseConfig;
+import br.com.jhonicosta.xapp_messenger.model.Usuario;
 
 public class ContatoFragment extends Fragment {
+
+    private RecyclerView listaContatos;
+    private ContatosAdapter adapter;
+    private List<Usuario> list = new ArrayList<>();
+    private DatabaseReference reference;
+    private ValueEventListener listener;
 
     public ContatoFragment() {
     }
@@ -17,6 +35,48 @@ public class ContatoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contato, container, false);
+        View view = inflater.inflate(R.layout.fragment_contato, container, false);
+
+        listaContatos = view.findViewById(R.id.listaContatos);
+        reference = FirebaseConfig.getFirebaseDatabase().child("usuarios");
+
+        adapter = new ContatosAdapter(list, getActivity());
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        listaContatos.setLayoutManager(layoutManager);
+        listaContatos.setHasFixedSize(true);
+        listaContatos.setAdapter(adapter);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        findAll();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        reference.removeEventListener(listener);
+    }
+
+    public void findAll() {
+        listener = reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot dados : dataSnapshot.getChildren()) {
+                    Usuario usuario = dados.getValue(Usuario.class);
+                    list.add(usuario);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
