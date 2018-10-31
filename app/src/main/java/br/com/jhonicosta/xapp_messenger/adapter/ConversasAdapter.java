0d19f843078cda711b304a525a7959e9e3e1a4a0
jhoni-costa <jhoni_costa@outlook.com>
2018-10-31 15:37:38 -0300

    Listando conversas

---
+++ app/src/main/java/br/com/jhonicosta/xapp_messenger/adapter/ConversasAdapter.java
@@ -0,0 +1,69 @@
+package br.com.jhonicosta.xapp_messenger.adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import br.com.jhonicosta.xapp_messenger.R;
+import br.com.jhonicosta.xapp_messenger.model.Conversa;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ConversasAdapter extends RecyclerView.Adapter<ConversasAdapter.MyViewHolder> {
+
+    private List<Conversa> conversas;
+    private Context context;
+
+    public ConversasAdapter(List<Conversa> conversas, Context context) {
+        this.conversas = conversas;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemLista = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.adapter_conversas, viewGroup, false);
+        return new MyViewHolder(itemLista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+        Conversa conversa = conversas.get(i);
+        myViewHolder.nome.setText(conversa.getUsuarioExibicao().getNome());
+        myViewHolder.ultima.setText(conversa.getUltimaMensagem());
+
+        if (conversa.getUsuarioExibicao().getFotoUsuario() != null) {
+            Glide.with(context).load(Uri.parse(conversa.getUsuarioExibicao().getFotoUsuario())).into(myViewHolder.foto);
+        } else {
+            myViewHolder.foto.setImageResource(R.drawable.padrao);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return conversas.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView foto;
+        TextView nome, ultima;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            foto = itemView.findViewById(R.id.adapter_foto_conversas);
+            nome = itemView.findViewById(R.id.adapter_nome_conversas);
+            ultima = itemView.findViewById(R.id.adapter_ultima_conversas);
+        }
+    }
+}
--- app/src/main/java/br/com/jhonicosta/xapp_messenger/fragments/ConversasFragment.java
+++ app/src/main/java/br/com/jhonicosta/xapp_messenger/fragments/ConversasFragment.java
@@ -2,15 +2,38 @@ package br.com.jhonicosta.xapp_messenger.fragments;
 
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.jhonicosta.xapp_messenger.R;
+import br.com.jhonicosta.xapp_messenger.adapter.ConversasAdapter;
+import br.com.jhonicosta.xapp_messenger.config.FirebaseConfig;
+import br.com.jhonicosta.xapp_messenger.controller.UsuarioController;
+import br.com.jhonicosta.xapp_messenger.model.Conversa;
 
 public class ConversasFragment extends Fragment {
 
+    private RecyclerView listaConversas;
+    private ConversasAdapter adapter;
+    private List<Conversa> list = new ArrayList<>();
+
+    private DatabaseReference reference;
+    private DatabaseReference conversasRef;
+    private ChildEventListener eventListener;
 
     public ConversasFragment() {
     }
@@ -18,7 +41,72 @@ public class ConversasFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_conversas, container, false);
+        View view = inflater.inflate(R.layout.fragment_conversas, container, false);
+
+        listaConversas = view.findViewById(R.id.listaConversas);
+
+        reference = FirebaseConfig.getFirebaseDatabase();
+
+        adapter = new ConversasAdapter(list, getActivity());
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        listaConversas.setLayoutManager(layoutManager);
+        listaConversas.setHasFixedSize(true);
+        listaConversas.setAdapter(adapter);
+
+        String idUsuario = new UsuarioController(getActivity()).getIdUser();
+
+        conversasRef = reference.child("conversas").child(idUsuario);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        recuperarConversas();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        conversasRef.removeEventListener(eventListener);
+    }
+
+    public void recuperarConversas() {
+        String idUsuario = new UsuarioController(getActivity()).getIdUser();
+
+        DatabaseReference conversasRef = reference.child("conversas").child(idUsuario);
+
+        eventListener = conversasRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Conversa conversa = dataSnapshot.getValue(Conversa.class);
+                list.add(conversa);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
 }
