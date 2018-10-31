636b513d12e272b9d01c696ebf7876158d96ef71
jhoni-costa <jhoni_costa@outlook.com>
2018-10-31 17:49:52 -0300

    Listando usuarios para adicionar a um grupo

--- app/src/main/java/br/com/jhonicosta/xapp_messenger/activities/GrupoActivity.java
+++ app/src/main/java/br/com/jhonicosta/xapp_messenger/activities/GrupoActivity.java
@@ -4,12 +4,34 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.jhonicosta.xapp_messenger.R;
+import br.com.jhonicosta.xapp_messenger.adapter.ContatosAdapter;
+import br.com.jhonicosta.xapp_messenger.config.FirebaseConfig;
+import br.com.jhonicosta.xapp_messenger.controller.UsuarioController;
+import br.com.jhonicosta.xapp_messenger.model.Usuario;
 
 public class GrupoActivity extends AppCompatActivity {
+
+    private RecyclerView rwMembros, rwMembrosSelecionados;
+    private ContatosAdapter adapter;
+    private List<Usuario> listMembros = new ArrayList<>();
+    private ValueEventListener valueEventListenerMembros;
+    private DatabaseReference usuariosRef;
+    private FirebaseUser usuarioAtual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +40,7 @@ public class GrupoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,6 +49,53 @@ public class GrupoActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        rwMembros = findViewById(R.id.rwMembros);
+        rwMembrosSelecionados = findViewById(R.id.rwMembrosSelecionados);
+        usuariosRef = FirebaseConfig.getFirebaseDatabase().child("usuarios");
+        usuarioAtual = new UsuarioController(this).getFirebaseUser();
+
+        adapter = new ContatosAdapter(listMembros, this);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        rwMembros.setLayoutManager(layoutManager);
+        rwMembros.setHasFixedSize(true);
+        rwMembros.setAdapter(adapter);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recuperarContatos();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        usuariosRef.removeEventListener(valueEventListenerMembros);
+    }
+
+    public void recuperarContatos() {
+        listMembros.clear();
+
+        valueEventListenerMembros = usuariosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot dados : dataSnapshot.getChildren()) {
+                    Usuario usuario = dados.getValue(Usuario.class);
+                    String emailUsuarioAtual = usuarioAtual.getEmail();
+                    if (!emailUsuarioAtual.equals(usuario.getEmail())) {
+                        listMembros.add(usuario);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
