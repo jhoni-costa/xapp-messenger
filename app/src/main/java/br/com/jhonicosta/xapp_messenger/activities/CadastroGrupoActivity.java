5bdbc0df64a5d90f5ce1c9a241fb3b628ec0352d
jhoni-costa <jhoni_costa@outlook.com>
2018-11-02 13:52:03 -0300

    Salvando imagem de grupo no storage

--- app/src/main/java/br/com/jhonicosta/xapp_messenger/activities/CadastroGrupoActivity.java
+++ app/src/main/java/br/com/jhonicosta/xapp_messenger/activities/CadastroGrupoActivity.java
@@ -1,6 +1,12 @@
 package br.com.jhonicosta.xapp_messenger.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,20 +14,30 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import br.com.jhonicosta.xapp_messenger.R;
 import br.com.jhonicosta.xapp_messenger.adapter.GrupoSelecionadoAdapter;
-import br.com.jhonicosta.xapp_messenger.helper.RecyclerItemClickListener;
+import br.com.jhonicosta.xapp_messenger.config.FirebaseConfig;
+import br.com.jhonicosta.xapp_messenger.model.Grupo;
 import br.com.jhonicosta.xapp_messenger.model.Usuario;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CadastroGrupoActivity extends AppCompatActivity {
+
+    private static final int CAMERA_ = 100;
+    private static final int GALERIA_ = 200;
 
     private List<Usuario> listaMembrosSelecionados = new ArrayList<>();
     private EditText nomeGrupo;
@@ -29,6 +45,10 @@ public class CadastroGrupoActivity extends AppCompatActivity {
     private CircleImageView imagemGrupo;
     private RecyclerView recyclerView;
     private GrupoSelecionadoAdapter grupoSelecionadoAdapter;
+
+    private Grupo grupo;
+
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +59,8 @@ public class CadastroGrupoActivity extends AppCompatActivity {
         toolbar.setSubtitle("Defina o nome");
         setSupportActionBar(toolbar);
 
+        grupo = new Grupo();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,11 +70,21 @@ public class CadastroGrupoActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        storageReference = FirebaseConfig.getFirebaseStorage();
         nomeGrupo = findViewById(R.id.txtNomeGrupo);
         txtTotalParticipantes = findViewById(R.id.txtTotalParticipantes);
-        imagemGrupo = findViewById(R.id.imageGrupo);
         recyclerView = findViewById(R.id.rvGrupo);
+        imagemGrupo = findViewById(R.id.imageGrupo);
+
+        imagemGrupo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, GALERIA_);
+                }
+            }
+        });
 
         if (getIntent().getExtras() != null) {
             List<Usuario> membros = (List<Usuario>) getIntent().getExtras().getSerializable("membros");
@@ -71,7 +103,50 @@ public class CadastroGrupoActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(grupoSelecionadoAdapter);
 
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Bitmap imagem = null;
+            try {
+
+                Uri localImagem = data.getData();
+                imagem = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagem);
+                if (imagem != null) {
+                    imagemGrupo.setImageBitmap(imagem);
+
+                    ByteArrayOutputStream boas = new ByteArrayOutputStream();
+                    imagem.compress(Bitmap.CompressFormat.JPEG, 70, boas);
+                    byte[] dataImage = boas.toByteArray();
+
+                    StorageReference imgRef = storageReference
+                            .child("imagens")
+                            .child("grupos")
+                            .child(grupo.getId() + ".jpeg");
+
+                    UploadTask uploadTask = imgRef.putBytes(dataImage);
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), R.string.upload_image_error, Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(getApplicationContext(), R.string.upload_image_sucess, Toast.LENGTH_SHORT).show();
+
+                            Uri url = taskSnapshot.getDownloadUrl();
+                            grupo.setFoto(url.toString());
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
---
+++ app/src/main/java/br/com/jhonicosta/xapp_messenger/model/Grupo.java
@@ -0,0 +1,57 @@
+package br.com.jhonicosta.xapp_messenger.model;
+
+import com.google.firebase.database.DatabaseReference;
+
+import java.io.Serializable;
+import java.util.List;
+
+import br.com.jhonicosta.xapp_messenger.config.FirebaseConfig;
+
+public class Grupo implements Serializable {
+
+    private String id;
+    private String nome;
+    private String foto;
+    private List<Usuario> membros;
+
+    public Grupo() {
+
+        DatabaseReference reference = FirebaseConfig.getFirebaseDatabase();
+        reference = reference.child("grupos");
+
+        String id = reference.push().getKey();
+        setId(id);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public List<Usuario> getMembros() {
+        return membros;
+    }
+
+    public void setMembros(List<Usuario> membros) {
+        this.membros = membros;
+    }
+}
